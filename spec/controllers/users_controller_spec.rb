ea99f1ffcdd25d5b3f50ec432822926f641a2207
simon <tong.xm520@gmail.com>
2015-01-12 00:06:43 +0800

    rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe UsersController do
-
+  render_views
+  fixtures :users
+  it "should render show template on show call when logged in" do
+    session[:user_id] = users(:sam).id
+    get 'show', :id=>users(:sam).id
+    response.should render_template('show')
+  end
 end
